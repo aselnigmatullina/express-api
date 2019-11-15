c270bd9c0f63be246db40d50b726a01042cc6381
aselnigmatullina <nogmatullina.asel2016@yandex.ru>
2019-11-15 21:28:46 +0300

    migrated to filter/map

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const errorNotFound = {error: 'id.not_found'};
 
 
 let nextId = 1;
-const posts = [
+let posts = [
     {id:nextId++, content: 'First post', likes: 0},
     {id:nextId++, content:'Second post', likes:0 },
 
@@ -29,33 +29,21 @@ server.get('/posts', (req,res) => {
 
 server.post('/posts', (req,res) => {
   const body = req.body;
-  if(body.id === 0){
-      posts.push({
-          id: nextId++,
-          content: body.content,
-          likes:0,
-      })
+  const id = body.id;
+  if(id === 0){
+  posts = [...posts, {id: nextId++,content: body.content,likes:0}];
       res.send(posts);
       return;
   }
-  const index = findPostIndexById(body.id);
-  if(index === -1){
-      res.status(404).send(errorNotFound);
-      return;
-  }
-  posts[index].content = body.content;
+  posts = posts.map(o => o.id !== id ? o : {...o,content: body.content});
   res.send(posts);
-
 });
 
 server.delete('/posts/:id', (req, res)  => {
 const id = Number(req.params.id);
-const index = findPostIndexById(id);
-    if(index === -1){
-    res.status(404).send(errorNotFound);
-    return;
-}
-posts.splice(index, 1);
+
+posts = posts.filter(o => o.id != id);
+
 res.send(posts);
 
 });
@@ -64,11 +52,13 @@ res.send(posts);
 server.post('/posts/:id/likes', (req,res) => {
 const id = Number(req.params.id);
 const index = findPostIndexById(id);
-    if(index === -1){
+if(index === -1){
     res.status(404).send(errorNotFound);
     return;
 }
-posts[index].likes++;
+posts = posts.map(o => o.id !== id ? o : {...o, likes: o.likes + 1});
+
+
 res.send(posts);
 
 
@@ -77,12 +67,14 @@ res.send(posts);
 
 server.delete('/posts/:id/likes', (req,res) => {
     const id = Number(req.params.id);
+
     const index = findPostIndexById(id);
         if(index === -1){
-        res.status(404).send(errorNotFound);
-        return;
-    }
-    posts[index].likes--;
+            res.status(404).send(errorNotFound);
+            return;
+        }
+    
+    posts = posts.map(o => o.id !== id ? o : {...o, likes: o.likes - 1});
     res.send(posts);
     
     
